2ca0005692cecc85a45b5f54ccb69f4cd6f60df5
Hyeokjae Lee (Nagle) <71566740+HyeokjaeLee@users.noreply.github.com>
2021-05-17 13:36:49 +0900

    ver 0.2.4
    deploy bug fix

--- out/_next/static/Uk_7aGS43KDxHl9D2LOV8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-96fe0e34958ebce3c9da.js"],"/_error":["static/chunks/pages/_error-a8b19aa03db10891b099.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
